57f0520c7b541893335842138ff4f131efb38021
m47h4r <m47h4r@gmail.com>
2020-05-20 15:17:08 +0430

    add contact list

--- front/src/components/ContactList.js
+++ front/src/components/ContactList.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Button from "./Button";
+import AddContact from "./AddContact";
 
 import "./ContactList.css";
 
 function ContactList(props) {
+	const [isAddContactPresent, setIsAddContactPresent] = useState(false);
+
+	const toggleAddContact = () => {
+		setIsAddContactPresent(!isAddContactPresent);
+	};
+
 	return (
-		<p>I am the Contact List</p>
+		<>
+			<div className="contact-list">
+				<div className="contact-list-header">
+					<div className="contact-list-header__title">title here</div>
+					<div className="contact-list-header__add-contact">
+						<Button
+							type="button"
+							onClick={toggleAddContact}
+							text={isAddContactPresent ? "Close" : "Add contact"}
+						/>
+					</div>
+				</div>
+				{isAddContactPresent ? (
+					<AddContact sessionCookie={props.sessionCookie} />
+				) : null}
+				<div className="contact-list-body">contacts will be here</div>
+			</div>
+		</>
 	);
 }
 
