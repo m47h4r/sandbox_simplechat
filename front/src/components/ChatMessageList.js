29301378b6f7375c4894c0a6f088595790243d2c
m47h4r <m47h4r@gmail.com>
2020-05-21 12:32:53 +0430

    add ChatMessageList component

---
+++ front/src/components/ChatMessageList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ChatMessageList(props) {
+	return (
+		<p>This is chat message list</p>
+	);
+}
+
+export default ChatMessageList;
--- front/src/pages/Chat.js
+++ front/src/pages/Chat.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import ChatInput from "../components/ChatInput";
+import ChatMessageList from "../components/ChatMessageList";
 
 function Chat(props) {
 	return (
 		<>
 		<p>I am chatting with: {props.renderProps.location.state.contact}</p>
+		<ChatMessageList />
 		<ChatInput />
 		</>
 	);
