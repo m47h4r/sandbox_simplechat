8700057ff0db61d3b85c999063e5b9cd141462d4
m47h4r <m47h4r@gmail.com>
2020-05-24 15:16:20 +0430

    refactor socket file to use async/await

--- back/socket/index.js
+++ back/socket/index.js
@@ -1,71 +1,59 @@
-// const User = require("../models/User")
-// const Message = require("../models/Message");
 const mongoose = require("mongoose");
 const Message = mongoose.model("Message");
 const User = mongoose.model("User");
 
+const getUser = async (sessionId) => {
+	try {
+		const user = await User.findOne({sessionSecret: sessionId }).exec();
+		if (!user) { return false }
+		return user;
+	} catch (e) {
+		return false;
+	}
+}
+
+const getMessages = async (userId, contactId) => {
+	try {
+		const messages = await Message.find(
+			{ $or: [
+				{ from: userId, to: contactId },
+				{ from: contactId, to: userId }
+			]}
+		)
+		.populate('from', 'name surname')
+		.populate('to', 'name surname')
+		.exec();
+		if (!messages) { return false; }
+		return messages;
+	} catch (e) {
+		return false;
+	}
+}
+
+const generateMessageList = (messages) => {
+	let dataToReturn = [];
+	for (let iterator in messages) {
+		let temp = {
+			_id: messages[iterator]._id,
+			date: messages[iterator].createdAt,
+			from: messages[iterator].from.name + " " + messages[iterator].from.surname,
+			to: messages[iterator].to.name + " " + messages[iterator].to.surname,
+			isSender: messages[iterator].from._id.toString() === user._id.toString(),
+			text: messages[iterator].text
+		}
+		dataToReturn.push(temp);
+	}
+	return dataToReturn;
+}
+
 function socket(io) {
 	io.on('connection', (socket) => {
-		// let m = new Message({
-			// text: "Hi how are you?",
-			// from: "5ec8d706ec3b56fb87cd2ad0",
-			// to: "5ec8d8b8ec3b56fb87cd2ad1"
-		// })
-		// m.save();
-		socket.on('get-chat-messages', (data, cb) => {
-			User.findOne(
-				{ sessionSecret: data.session_id },
-				(error, user) => {
-					if (!user || error) {
-						cb({result: false, error: "Session not valid"});
-						// socket.emit('return-chat-messages',
-							// { 
-								// result: false,
-								// error: "User session is not valid"
-							// }
-						// );
-					}
-					Message.find(
-						{ $or: [
-							{ from: user._id, to: data.contact_id },
-							{ from: data.contact_id, to: user._id}
-							]
-						})
-						.populate('from', 'name surname')
-						.populate('to', 'name surname')
-						.exec((error, messages) => {
-							if (!messages || error) {
-								cb({result: false, error: "Session not valid"});
-								// socket.emit('return-chat-messages',
-									// { 
-										// result: false,
-										// error: "User session is not valid"
-									// }
-								// );
-							}
-							let dataToReturn = [];
-							for (let iterator in messages) {
-								let temp = {
-									_id: messages[iterator]._id,
-									date: messages[iterator].createdAt,
-									from: messages[iterator].from.name + " " + messages[iterator].from.surname,
-									to: messages[iterator].to.name + " " + messages[iterator].to.surname,
-									isSender: messages[iterator].from._id.toString() === user._id.toString(),
-									text: messages[iterator].text
-								}
-								dataToReturn.push(temp);
-							}
-							cb({result: true, data: dataToReturn});
-							// socket.emit('return-chat-messages',
-								// {
-									// result: true,
-									// data: message
-								// }
-							// );
-						}
-					);
-				}
-			);
+		socket.on('get-chat-messages', async (data, cb) => {
+			const user = await getUser(data.session_id);
+			if (!user) { return cb({result: false, error: "Session not valid"}); }
+			const messages = await getMessages(user._id, data.contact_id)
+			const generatedMessageList = generateMessageList(messages);
+			cb({result: true, data: generatedMessageList });
 		});
 	});
 }
--- front/src/components/ChatMessageList.js
+++ front/src/components/ChatMessageList.js
@@ -3,36 +3,37 @@ import React, { useEffect } from "react";
 import "./ChatMessageList.css";
 
 function ChatMessageList(props) {
-
 	// useEffect(() => {
-		// 
+	//
 	// }, [props.data])
 
 	const renderMessages = () => {
 		return (
 			<>
-			{props.messages.map((message) => (
-				message.isSender ?
-				<div className="chat-message chat-message--sender">
-					{message.text}
-				</div>
-				:
-				<div className="chat-message chat-message--receiver">
-					{message.text}
-				</div>
-			))}
+				{props.messages.map((message) =>
+					message.isSender ? (
+						<div
+							className="chat-message chat-message--sender"
+							key={message._id}
+						>
+							{message.text}
+						</div>
+					) : (
+						<div
+							className="chat-message chat-message--receiver"
+							key={message._id}
+						>
+							{message.text}
+						</div>
+					)
+				)}
 			</>
 		);
-	}
+	};
 
 	return (
 		<div className="chat-message-list">
-			{props.messages
-			?
-				renderMessages()
-			:
-				<p>There are no messages.</p>
-			}
+			{props.messages ? renderMessages() : <p>There are no messages.</p>}
 		</div>
 	);
 }
