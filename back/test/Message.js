2f0685ac11ee0b444cb298f518aa55611a38471f
m47h4r <m47h4r@gmail.com>
2020-06-12 14:03:26 +0430

    add eslint mocha env and fixed the test

--- back/test/Message.js
+++ back/test/Message.js
@@ -1,25 +1,37 @@
 const expect = require("chai").expect;
-
+const mongoose = require("mongoose");
 const Message = require("../models/Message");
 
 describe("model:Message", () => {
 	describe("Emptiness", () => {
 		it("should be invalid if 'text' is empty", (done) => {
-			const message = new Message();
+			const message = new Message({
+				text: "",
+				from: mongoose.Types.ObjectId("000000000000000000000000"),
+				to: mongoose.Types.ObjectId("000000000000000000000000"),
+			});
 			message.validate((err) => {
 				expect(err.errors.text).to.exist;
 				done();
 			});
 		});
 		it("should be invalid if 'from' is empty", (done) => {
-			const message = new Message();
+			const message = new Message({
+				text: "something",
+				from: "",
+				to: mongoose.Types.ObjectId("000000000000000000000000"),
+			});
 			message.validate((err) => {
 				expect(err.errors.from).to.exist;
 				done();
 			});
 		});
 		it("should be invalid if 'to' is empty", (done) => {
-			const message = new Message();
+			const message = new Message({
+				text: "something",
+				from: mongoose.Types.ObjectId("000000000000000000000000"),
+				to: "",
+			});
 			message.validate((err) => {
 				expect(err.errors.to).to.exist;
 				done();
