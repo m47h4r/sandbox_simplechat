203bb1cf9291a402b645deb072b542efb27d31db
m47h4r <m47h4r@gmail.com>
2020-06-21 14:47:31 +0430

    add supertest and tests for session routes

---
+++ back/test/route/session.js
@@ -0,0 +1,37 @@
+const supertest = require("supertest");
+const express = require("express");
+
+const userRouter = require("../../routes/user");
+const sessionRouter = require("../../routes/session");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use("/user", userRouter);
+app.use("/session", sessionRouter);
+
+describe("route:session", function () {
+	describe("GET /session/check", function () {
+		it("should respond with json", function (done) {
+			supertest(app)
+				.get("/session/check")
+				.set("Accept", "application/json")
+				.expect("Content-Type", /json/)
+				.expect(200, done);
+		});
+	});
+
+	describe("POST /session/update", function () {
+		it("should respond with json", function (done) {
+			supertest(app)
+				.post('/session/update')
+				.send({claimedSessionSecret: null})
+				.set('Accept', 'application/json')
+				.expect('Content-Type', /json/)
+				.expect(200)
+				.end(function(err) {
+					if (err) return done(err);
+					done();
+				});
+		});
+	});
+});
