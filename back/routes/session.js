aabf0fe347708e321e5c5eaafde1d76e25dddded
m47h4r <m47h4r@gmail.com>
2020-06-25 13:51:16 +0430

    add docs for session router

--- back/routes/session.js
+++ back/routes/session.js
@@ -1,14 +1,56 @@
+/**
+ * Express router providing session end points
+ * @module routers/session
+ * @requires express
+ */
+
+/**
+ * Express module
+ * @const
+ */
 const express = require("express");
+
+/**
+ * Express router to mount the session related functions on
+ * @type {object}
+ * @const
+ * @namespace sessionRouter
+ */
 const router = express.Router();
+
+/**
+ * Mongoose module
+ * @const
+ */
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
+/**
+ * Route to check session
+ * @name get/session/check
+ * @function
+ * @memberof module:routers/session~sessionRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @returns {json} - Status of session being valid or not
+ */
 router.get("/check", async (request, response) => {
 	const claimedSession = request.headers.claimedsession;
 	const isSessionValid = await User.checkSession(claimedSession);
 	response.json({ result: isSessionValid });
 });
 
+/**
+ * Route to update session
+ * @name post/session/update
+ * @function
+ * @memberof module:routers/session~sessionRouter
+ * @inner
+ * @param {string} path - Express path
+ * @param {callback} middleware - Express middleware.
+ * @returns {json} - Status of session being updated
+ */
 router.post("/update", async (request, response) => {
 	const claimedSession = request.body.claimedSessionSecret;
 	const isSessionUpdated = await User.updateSession(claimedSession);
